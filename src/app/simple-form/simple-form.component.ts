db065eb8a43d82ba1116e48fd1b7605839c6a721
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-11-17 16:18:54 -0800

    use events and refd

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,14 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-  <p>
-    simple-form Works!
-  </p>
+  <div>
+    <input #myInput type="text">
+    <button (click)="onClick($event, myInput.value)">Confirm</button>
+  </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
 
+  onClick(event, value) {
+    console.log(event);
+    console.log(value);
+
+  }
   constructor() { }
 
   ngOnInit() {
