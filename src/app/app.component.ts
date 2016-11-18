bc6fe1d2c22379a84792bd94567f1de7e2a4e88e
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-11-17 18:49:45 -0800

    add service Mail and injected it to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MailService } from './mail.service';
 
 @Component({
   selector: 'app-root',
-  template: `<div>
-<app-simple-form></app-simple-form>
-  </div>`
+  template: `
+  <div>
+    <app-simple-form></app-simple-form>
+
+    {{mail.messege}}
+  </div>
+  `
 
 })
 export class AppComponent {
   title = 'Lets learn more!';
+
+  constructor(private mail:MailService){}
 }
---
+++ src/app/mail.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MailService {
+
+  messege = "You've got mail";
+  constructor() { }
+
+}
